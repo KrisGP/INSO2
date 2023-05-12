df1f7c6ae616bfa39169291b67edd622ca9175a6
KrisGP <55236949+KrisGP@users.noreply.github.com>
2023-05-12 23:39:45 +0200

    Update UsuarioController.java
    Arreglado conflicto.

--- src/main/java/istrabajo/controller/UsuarioController.java
+++ src/main/java/istrabajo/controller/UsuarioController.java
@@ -19,67 +19,21 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 /**
  *
- */
+ */    
 @Named
 @ViewScoped
-public class UsuarioController implements Serializable {
-        
+public class UsuarioController implements Serializable{
+    public static final String TIPOUSUARIO = "USUARIO";
+    public static final String TIPOADMINISTRADOR = "ADMINISTRADO";
+    public static final String TIPODEPENDIENTE = "DEPENDIENTE";
+    
     @EJB
-    private UsuarioFacadeLocal usuarioEjb;    
+    private UsuarioFacadeLocal usuarioEjb;
     @EJB
     private TarjetaFacadeLocal tarjetaEjb;
-    
     private Usuario usuario;
-    private Tarjeta tarjeta;
-    
-    @PostConstruct
-    public void init() {
-        usuario = new Usuario();
-        tarjeta = new Tarjeta();
-    }
         
-    /**
-     * Registra la tarjeta introducida a al usuario que tiene la sesión abierta
-     * Primero valida los datos introducidos sean correctos
-     * 
-     */
-    public void registrarTarjeta() {
-       if(tarjeta.getTarjetasCreditoCol().length() == 16 
-          && (tarjeta.getTarjetasCreditoCol().charAt(0) == TarjetaController.NUMEROVISA
-          || tarjeta.getTarjetasCreditoCol().charAt(0) == TarjetaController.NUMEROMASTERCARD)) {
-            
-            //Validar que la tarjeta sea correcta
-            String mesFormateado = null;
-            try {
-                //Validamos que la tarjeta sea correcta
-                mesFormateado = TarjetaController.validarYFormatearMesCaducidad(this.tarjeta.getMesCaduca());
-                int anyoFormateado = Integer.parseInt(this.tarjeta.getAnyoCaduca());
-
-                //Comprobar la tarjeta. Comprobar de que tipo es. Incluirlo al final
-                String tipoTarjeta;
-                tipoTarjeta = TarjetaController.getTipoTarjeta(tarjeta);
-
-                //Comprobamos que no exista en la base de datos
-                if(tarjetaEjb.getTarjeta(tarjeta.getTarjetasCreditoCol()) != null) {
-                    //Si existe, mostramos un mensaje de alerta al usuario
-                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ya tienes esta tarjeta registrada", null));
-                } else {
-                    //Si no existe, introducimos la tarjeta al usuario que tenga la sesión abierta
-                    usuario = usuarioEjb.find(SesionUsuario.getInstance().getIdUsuario());
-                    tarjeta.setUsuario(usuario);
-                    usuario.addTarjeta(tarjeta);
-                    usuarioEjb.edit(usuario);
-
-                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrada la tarjeta de tipo " + tipoTarjeta, null));
-                }
-
-            } catch(NumberFormatException e) {
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en el formato de la fecha. Revisela", null));
-            }
-        } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en el número de la tarjeta. Debe de ser una tarjeta Visa o Mastercard", null));
-        }
-    }
+    private Tarjeta tarjeta;
     
     public UsuarioFacadeLocal getUsuarioEjb() {
         return usuarioEjb;
@@ -96,50 +50,13 @@ public class UsuarioController implements Serializable {
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-
+        
     public Tarjeta getTarjeta() {
         return tarjeta;
     }
 
     public void setTarjeta(Tarjeta tarjeta) {
         this.tarjeta = tarjeta;
-    }
-    
-    
-public class UsuarioController implements Serializable{
-    public static final String TIPOUSUARIO = "USUARIO";
-    public static final String TIPOADMINISTRADOR = "ADMINISTRADO";
-    public static final String TIPODEPENDIENTE = "DEPENDIENTE";
-    
-    @EJB
-    private UsuarioFacadeLocal usuarioEjb;
-    private Usuario usuario;
-    
-@Named
-@ViewScoped
-public class UsuarioController implements Serializable{
-    public static final String TIPOUSUARIO = "USUARIO";
-    public static final String TIPOADMINISTRADOR = "ADMINISTRADO";
-    public static final String TIPODEPENDIENTE = "DEPENDIENTE";
-    
-    @EJB
-    private UsuarioFacadeLocal usuarioEjb;
-    private Usuario usuario;
-
-    public UsuarioFacadeLocal getUsuarioEjb() {
-        return usuarioEjb;
-    }
-
-    public void setUsuarioEjb(UsuarioFacadeLocal usuarioEjb) {
-        this.usuarioEjb = usuarioEjb;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
     }
     
     public void iniciarSesion() throws IOException{
@@ -193,11 +110,55 @@ public class UsuarioController implements Serializable{
         // Si ha pasado todas las comprobaciones, el DNI es válido
         return true;
     }
+    
+     /**
+     * Registra la tarjeta introducida a al usuario que tiene la sesión abierta
+     * Primero valida los datos introducidos sean correctos
+     * 
+     */
+    public void registrarTarjeta() {
+       if(tarjeta.getTarjetasCreditoCol().length() == 16 
+          && (tarjeta.getTarjetasCreditoCol().charAt(0) == TarjetaController.NUMEROVISA
+          || tarjeta.getTarjetasCreditoCol().charAt(0) == TarjetaController.NUMEROMASTERCARD)) {
+            
+            //Validar que la tarjeta sea correcta
+            String mesFormateado = null;
+            try {
+                //Validamos que la tarjeta sea correcta
+                mesFormateado = TarjetaController.validarYFormatearMesCaducidad(this.tarjeta.getMesCaduca());
+                int anyoFormateado = Integer.parseInt(this.tarjeta.getAnyoCaduca());
+
+                //Comprobar la tarjeta. Comprobar de que tipo es. Incluirlo al final
+                String tipoTarjeta;
+                tipoTarjeta = TarjetaController.getTipoTarjeta(tarjeta);
+
+                //Comprobamos que no exista en la base de datos
+                if(tarjetaEjb.getTarjeta(tarjeta.getTarjetasCreditoCol()) != null) {
+                    //Si existe, mostramos un mensaje de alerta al usuario
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ya tienes esta tarjeta registrada", null));
+                } else {
+                    //Si no existe, introducimos la tarjeta al usuario que tenga la sesión abierta
+                    usuario = usuarioEjb.find(SesionUsuario.getInstance().getIdUsuario());
+                    tarjeta.setUsuario(usuario);
+                    usuario.addTarjeta(tarjeta);
+                    usuarioEjb.edit(usuario);
+
+                    FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrada la tarjeta de tipo " + tipoTarjeta, null));
+                }
+
+            } catch(NumberFormatException e) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en el formato de la fecha. Revisela", null));
+            }
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Error en el número de la tarjeta. Debe de ser una tarjeta Visa o Mastercard", null));
+        }
+    }
 
 
     @PostConstruct
-    public void init(){
+    public void init() {
         usuario = new Usuario();
+        tarjeta = new Tarjeta();
     }
     
     public void guardar(){
